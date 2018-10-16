634422e933196ab8817c4104f5b303e9361bfcb0
cuzz1 <cuzz1234@163.com>
2018-10-16 20:57:41 +0800

    variables

--- src/github.com/cuzz/go_learning/02_vars/main.go
+++ src/github.com/cuzz/go_learning/02_vars/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	// MAIN TYPES
 	// string
@@ -11,6 +15,35 @@ func main() {
 	// rune - alias for int32
 	// float32 float64
 	// complex64 complex128
+
+	// test01()
+	test02()
+
 }
+
+func test02()  {
+	// Shorthand
+	name := "cuzz"
+	size := 1.1
+	var size2 float32 = 2.2
+	fmt.Println(name, size)
+	fmt.Printf("Type of name is a %T\n", name)
+	fmt.Printf("Type of size is a %T\n", size)
+	fmt.Printf("Type of size2 is a %T\n", size2)
+}
+
+
+func test01() {
+	// Using var
+	var name = "cuzz"	     // var name string = "cuzz"
+	var age int32 = 20		 // var age int = 20
+	var isCool = true
+
+	sex, email := "male", "cuzz1234@163.com"
+
+	fmt.Println(name, age, isCool, sex, email)
+	fmt.Printf("Type of name is a %T, Type of age is a %T", name, age)
+}
+
 
 
