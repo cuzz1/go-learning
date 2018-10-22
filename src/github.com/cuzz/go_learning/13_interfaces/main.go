a61923a800c2cdc539bd89f4bcbe62d77658fdd5
cuzz1 <cuzz1234@163.com>
2018-10-22 18:26:25 +0800

    interfaces

---
+++ src/github.com/cuzz/go_learning/13_interfaces/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Define interface
+type Shape interface {
+	area() float64
+}
+
+type Circle struct {
+	x, y, radius float64
+}
+
+type Rectangle struct {
+	width, height float64
+}
+
+func (c Circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (r Rectangle) area() float64 {
+	return r.height * r.width
+}
+
+func getArea(s Shape) float64 {
+	return s.area()
+}
+
+func main()  {
+	circle := Circle{x:0, y:0, radius:5}
+	rectangle := Rectangle{width:3, height:4}
+
+	fmt.Println(getArea(circle))
+	fmt.Println(getArea(rectangle))
+
+}
+
+
