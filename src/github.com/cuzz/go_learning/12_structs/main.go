126026e77a9a5c4f6ba61e054a04c9c9e5725b4f
cuzz1 <cuzz1234@163.com>
2018-10-22 18:16:28 +0800

    struct

--- src/github.com/cuzz/go_learning/12_structs/main.go
+++ src/github.com/cuzz/go_learning/12_structs/main.go
@@ -11,6 +11,26 @@ type Persion struct {
 	age int
 }
 
+// Greeting method (value receiver)
+func (p Persion) greet() string {
+	return "Hello, my name is " + p.firstName + " " + p.lastName
+}
+
+// hasBirthday method (pointer receiver)
+func (p *Persion) hasBirthday()  {
+	p.age++
+}
+
+// getMarried (pointer receiver)
+func (p *Persion) getMarried(spouseLastName string)  {
+	if p.gender == "m" {
+		return
+	} else {
+		p.lastName = spouseLastName
+	}
+
+}
+
 func main() {
 	// Init person using struct
 	persion1 := Persion{lastName:"Lee", city:"Wuhan", gender:"f", age:20}
@@ -20,6 +40,15 @@ func main() {
 	persion2 := Persion{"cuxx", "lee", "wuhan", "f", 20}
 	fmt.Println(persion2)
 	fmt.Println(persion2.firstName)
+
+	// Gretting
+	fmt.Println(persion2.greet())
+
+	persion2.getMarried("xxx")
+	fmt.Println(persion2.greet())
+
+
+
 
 }
 
